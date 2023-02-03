fa7e220794ffed28f20360d3ba373eb52ec6d872
Tatsuya Katagiri <tatsuya.katagiri@divx.co.jp>
2023-02-03 15:33:02 +0900

    post-memo

--- backend/src/app/controllers/api/v1/memo/memos_controller.rb
+++ backend/src/app/controllers/api/v1/memo/memos_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::Memo::MemosController < ApplicationController
+  def create
+    content = memo_params["content"]
+    memo = Memo.new(content: content)
+    memo.save!
+    render json: { status: 200 }
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:content)
+  end
 end
--- frontend/src/pages/memo/new.tsx
+++ frontend/src/pages/memo/new.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
+import axios from "axios"
+import Link from "next/link"
+
 const NewMemo = () => {
+  const [text, setText] = useState("");
+  const params = {content: text}
+  // console.log(params)
+  const createMemo = async () => {
+    try {
+      const result = await axios.post("http://localhost:80/api/v1/memo/memos", params)
+      console.log(result.data)
+      
+      const memoForm = document.getElementById('memo-form') as HTMLInputElement
+      memoForm.value = ''
+    } catch(e) {
+      console.log(e)
+    }
+   
+  }
   return (
     <>
       <h1>新規メモ作成</h1>
-      <div>
-        <input type="text" />
-        <button>保存</button>
+
+      <div onSubmit={createMemo}>
+        <input type="text" id="memo-form" value={text} onChange={(event) => setText(event.target.value)} />
+        <button onClick={createMemo}>保存</button>
       </div>
+
+      <Link href="/">
+        home
+      </Link>
     </>
   )
 }
